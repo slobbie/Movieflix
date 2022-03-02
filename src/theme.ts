ab88f7a04664c4b43090ca8c8a075c9db5bdecf7
slobbie <haeru9410@gmail.com>
2022-03-03 00:45:33 +0900

    :tada: Start  프로젝트 시작

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: '#333',
+  accentColor: '#4b7bec',
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: 'whitesmoke',
+  textColor: 'black',
+  accentColor: '#4b7bec',
+};
