999ddf46e78dac62c427f780cdd6ffe69678b521
slobbie <haeru9410@gmail.com>
2022-03-09 00:55:56 +0900

    :sparkles: Feat create Movie Modal

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,14 @@
+import { useLocation, useSearchParams } from 'react-router-dom';
+
 const Search = () => {
+  // url 의 data를 캐치해오는 훅
+  // const location = useLocation();
+  // console.log(location);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const keyword = searchParams.get('keyword');
+  console.log(keyword);
+
   return (
     <div>
       <h1>3</h1>
